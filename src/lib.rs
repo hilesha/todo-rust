51154574fd6d74f3a5c9d820a59732ebed71ed08
hilesh aswani <hilesh.aswani@startv.com>
2020-11-04 13:26:34 +0530

    add insert functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,51 @@
 use std::fs::File;
+use std::fs::OpenOptions;
 use std::io::prelude::*;
 use std::io::BufReader;
-pub fn get_todo_list(path: &str) -> Result<Vec<String>, String> {
-    let todo_file = File::open(path).unwrap();
-    let todo_file_reader = BufReader::new(todo_file);
-    let mut todo_items = Vec::new();
-    for item in todo_file_reader.lines() {
-        match item {
-            Ok(item) => todo_items.push(format_todo_item(item)),
-            Err(error) => return Err(format!("error reading line {}", error)),
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error(transparent)]
+    FileReadError(#[from] std::io::Error),
+    #[error("invalid command specified in the input")]
+    InvalidInput(String),
+}
+
+pub enum TaskStatus {
+    Added,
+    Deleted,
+    Marked,
+}
+
+type Result<T, E = Error> = std::result::Result<T, E>;
+
+pub fn get_todo_list(path: &str) -> Result<Vec<String>> {
+    match File::open(path) {
+        Ok(todo_file) => {
+            let todo_file_reader = BufReader::new(todo_file);
+            let mut todo_items = Vec::new();
+            for item in todo_file_reader.lines() {
+                match item {
+                    Ok(item) => todo_items.push(format_todo_item(item)),
+                    Err(error) => return Err(Error::FileReadError(error)),
+                }
+            }
+            Ok(todo_items)
         }
+        Err(error) => return Err(Error::FileReadError(error)),
     }
-    Ok(todo_items)
+}
+
+pub fn insert_todo(path: &str, todo_item: String) -> Result<TaskStatus> {
+    match OpenOptions::new().append(true).open(path) {
+        Ok(ref mut file_writer) => {
+            let mut item_write_to_file = todo_item;
+            item_write_to_file.push('\n');
+            file_writer.write(item_write_to_file.as_bytes());
+            Ok(TaskStatus::Added)
+        }
+        Err(error) => Err(Error::FileReadError(error)),
+    }
 }
 
 fn format_todo_item(todo_item: String) -> String {
@@ -24,7 +58,7 @@ mod tests {
 
     #[test]
     fn can_run_the_app() {
-        let path = "/Users/hilesha/self/rust/todo/src/todos.data";
+        let path = "/aUsers/hilesha/self/rust/todo/src/todos.data";
         let todo_items = get_todo_list(path).unwrap();
         assert_eq!(2, todo_items.len());
         assert_eq!("[ ] - make a todo list application", todo_items[0]);
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,56 @@
+use std::convert::TryFrom;
 use structopt::StructOpt;
 use todo;
+use todo::{Error, TaskStatus};
 
 #[derive(Debug, StructOpt)]
 struct Opt {
-    #[structopt(short = "f", long = "file", required = true)]
+    #[structopt(short = "f", long = "file", env = "TASK_ARCHIVE")]
     file_path: String,
+    #[structopt(name = "command", short = "c", long = "command")]
+    command: String,
+    #[structopt(short = "t", long = "task", default_value = "")]
+    todo: String,
+}
+
+enum Command {
+    List,
+    Add,
+    Delete,
+}
+
+impl TryFrom<&str> for Command {
+    type Error = Error;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value {
+            "add" => Ok(Command::Add),
+            "list" => Ok(Command::List),
+            "delete" => Ok(Command::Delete),
+            _ => Err(Error::InvalidInput(String::from(value))),
+        }
+    }
 }
 
 pub fn main() {
     let opt = Opt::from_args();
-    let todo_items = todo::get_todo_list(&opt.file_path).unwrap();
-    for item in todo_items {
-        println!("{}", item);
+    match Command::try_from(opt.command.as_ref()) {
+        Ok(command) => match command {
+            Command::List => match todo::get_todo_list(&opt.file_path) {
+                Ok(todo_items) => {
+                    for item in todo_items {
+                        println!("{}", item);
+                    }
+                }
+                Err(error) => println!("{}", error),
+            },
+            Command::Add => match todo::insert_todo(&opt.file_path, opt.todo) {
+                Ok(TaskStatus::Added) => println!("task added successfully"),
+                Ok(_) => println!("invalid status"),
+                Err(error) => println!("{}", error),
+            },
+            Command::Delete => println!("yet to be added"),
+            _ => println!("come back later"),
+        },
+        Err(error) => println!("error encountered"),
     }
 }
